bea566555eb75bde31d9525d909f9c496a679a87
Jin Qing <jq2019a@163.com>
2020-11-13 15:58:01 +0800

    简化代码

--- core/BehaviorTree.go
+++ core/BehaviorTree.go
@@ -296,8 +296,7 @@ func (this *BehaviorTree) Tick(target interface{}, blackboard *Blackboard) b3.St
 
 	/* CLOSE NODES FROM LAST TICK, IF NEEDED */
 	var lastOpenNodes = blackboard._getTreeData(this.id).OpenNodes
-	var currOpenNodes []IBaseNode
-	currOpenNodes = append(currOpenNodes, tick._openNodes...)
+	var currOpenNodes = tick._openNodes
 
 	// does not close if it is still open in this tick
 	var start = 0
